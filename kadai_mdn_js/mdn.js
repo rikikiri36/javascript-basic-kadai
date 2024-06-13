5752bce9342c6e346b0e8d109f626c16b473e252
rikiriki36 <rikirine_oiko@yahoo.co.jp>
2024-06-13 19:52:43 +0900

    MDN課題

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+let today = new Date();
+
+//console.log(today);
+
+let year = today.getFullYear();
+let month = today.getMonth();
+let date = today.getDate();
+
+console.log(year + '年' + month + '月' + date + '日');
